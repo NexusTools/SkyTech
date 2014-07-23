da4b9efa80905eb1c65b6dcb633f2a2d4dd35dc6
aerospark <lukefay12@gmail.com>
2014-07-23 16:54:49 -0700

    Fixed textures

--- src/main/java/com/nexustools/replicator/BReplicator.java
+++ src/main/java/com/nexustools/replicator/BReplicator.java
@@ -20,7 +20,7 @@ import net.minecraft.util.Icon;
 import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
 
-public class BReplicator extends BlockContainer {
+public class BReplicator extends BlockContainer { // code taken from BlockFurnace
 
     /**
      * Is the random generator used by furnace to drop the inventory contents in
@@ -43,12 +43,18 @@ public class BReplicator extends BlockContainer {
     private Icon furnaceIconTop;
     @SideOnly(Side.CLIENT)
     private Icon furnaceIconFront;
+    @SideOnly(Side.CLIENT)
+    private Icon[] icons = new Icon[8];
+    
 
     protected BReplicator(int par1, boolean par2) {
         super(par1, Material.rock);
         this.isActive = par2;
         this.setCreativeTab(CreativeTabs.tabRedstone);
     }
+    
+//    @SideOnly(Side.CLIENT)
+//    this
 
     /**
      * Returns the ID of the items to drop on destruction.
@@ -103,7 +109,8 @@ public class BReplicator extends BlockContainer {
      * Args: side, metadata
      */
     public Icon getIcon(int par1, int par2) {
-        return par1 == 1 ? this.furnaceIconTop : (par1 == 0 ? this.furnaceIconTop : (par1 != par2 ? this.blockIcon : this.furnaceIconFront));
+        return icons[par1];
+//        return par1 == 1 ? this.furnaceIconTop : (par1 == 0 ? this.furnaceIconTop : (par1 != par2 ? this.blockIcon : this.furnaceIconFront));
     }
 
     @SideOnly(Side.CLIENT)
@@ -113,10 +120,12 @@ public class BReplicator extends BlockContainer {
      * needs with the given IconRegister. This is the only chance you get to
      * register icons.
      */
-    public void registerIcons(IconRegister par1IconRegister) {
-        this.blockIcon = par1IconRegister.registerIcon("furnace_side");
-        this.furnaceIconFront = par1IconRegister.registerIcon(this.isActive ? "furnace_front_on" : "furnace_front_off");
-        this.furnaceIconTop = par1IconRegister.registerIcon("furnace_top");
+    public void registerIcons(IconRegister r) {
+        this.blockIcon = r.registerIcon("replicatormod:replicator");
+        for(int i = 0; i < 8; i++)icons[i]  = blockIcon;
+        this.furnaceIconFront = r.registerIcon(this.isActive ? "furnace_front_on" : "furnace_front_off");
+        this.furnaceIconTop = r.registerIcon("furnace_top");
+
     }
 
     /**
@@ -135,84 +144,6 @@ public class BReplicator extends BlockContainer {
         return true;
     }
 
-//    public boolean onBlockActivated(World par1World, int par2, int par3, int par4, EntityPlayer par5EntityPlayer, int par6, float par7, float par8, float par9)
-//    {
-//        System.out.println("ReplicatorBlockActivated");
-//        if (par1World.isRemote)
-//        {
-//            return true;
-//        }
-//        else
-//        {
-//            TEReplicator tileentityfurnace = (TEReplicator)par1World.getBlockTileEntity(par2, par3, par4);
-//
-//            if (tileentityfurnace != null)
-//            {
-////                par5EntityPlayer.displayGUIFurnace(tileentityfurnace);
-//                System.out.println("BReplicator: tileentity != null");
-//                par5EntityPlayer.openGui(ReplicatorMod.instance, 0, par1World, par2, par3, par4);
-//            }else{
-//                System.out.println("BReplicator: tileentity == null");
-//            }
-//
-//            return true;
-//        }
-//    }
-    /**
-     * Update which block ID the furnace is using depending on whether or not it
-     * is burning
-     */
-    public static void updateFurnaceBlockState(boolean par0, World par1World, int par2, int par3, int par4) {
-        int l = par1World.getBlockMetadata(par2, par3, par4);
-        TileEntity tileentity = par1World.getBlockTileEntity(par2, par3, par4);
-        keepFurnaceInventory = true;
-
-        if (par0) {
-            par1World.setBlock(par2, par3, par4, Block.furnaceBurning.blockID);
-        } else {
-            par1World.setBlock(par2, par3, par4, Block.furnaceIdle.blockID);
-        }
-
-        keepFurnaceInventory = false;
-        par1World.setBlockMetadataWithNotify(par2, par3, par4, l, 2);
-
-        if (tileentity != null) {
-            tileentity.validate();
-            par1World.setBlockTileEntity(par2, par3, par4, tileentity);
-        }
-    }
-
-    @SideOnly(Side.CLIENT)
-
-    /**
-     * A randomly called display update to be able to add particles or other
-     * items for display
-     */
-    public void randomDisplayTick(World par1World, int par2, int par3, int par4, Random par5Random) {
-        if (this.isActive) {
-            int l = par1World.getBlockMetadata(par2, par3, par4);
-            float f = (float) par2 + 0.5F;
-            float f1 = (float) par3 + 0.0F + par5Random.nextFloat() * 6.0F / 16.0F;
-            float f2 = (float) par4 + 0.5F;
-            float f3 = 0.52F;
-            float f4 = par5Random.nextFloat() * 0.6F - 0.3F;
-
-            if (l == 4) {
-                par1World.spawnParticle("smoke", (double) (f - f3), (double) f1, (double) (f2 + f4), 0.0D, 0.0D, 0.0D);
-                par1World.spawnParticle("flame", (double) (f - f3), (double) f1, (double) (f2 + f4), 0.0D, 0.0D, 0.0D);
-            } else if (l == 5) {
-                par1World.spawnParticle("smoke", (double) (f + f3), (double) f1, (double) (f2 + f4), 0.0D, 0.0D, 0.0D);
-                par1World.spawnParticle("flame", (double) (f + f3), (double) f1, (double) (f2 + f4), 0.0D, 0.0D, 0.0D);
-            } else if (l == 2) {
-                par1World.spawnParticle("smoke", (double) (f + f4), (double) f1, (double) (f2 - f3), 0.0D, 0.0D, 0.0D);
-                par1World.spawnParticle("flame", (double) (f + f4), (double) f1, (double) (f2 - f3), 0.0D, 0.0D, 0.0D);
-            } else if (l == 3) {
-                par1World.spawnParticle("smoke", (double) (f + f4), (double) f1, (double) (f2 + f3), 0.0D, 0.0D, 0.0D);
-                par1World.spawnParticle("flame", (double) (f + f4), (double) f1, (double) (f2 + f3), 0.0D, 0.0D, 0.0D);
-            }
-        }
-    }
-
     /**
      * Returns a new instance of a block's tile entity class. Called on placing
      * the block.
@@ -221,107 +152,4 @@ public class BReplicator extends BlockContainer {
         return new TEReplicator();
     }
 
-    /**
-     * Called when the block is placed in the world.
-     */
-    public void onBlockPlacedBy(World par1World, int par2, int par3, int par4, EntityLivingBase par5EntityLivingBase, ItemStack par6ItemStack) {
-        int l = MathHelper.floor_double((double) (par5EntityLivingBase.rotationYaw * 4.0F / 360.0F) + 0.5D) & 3;
-
-        if (l == 0) {
-            par1World.setBlockMetadataWithNotify(par2, par3, par4, 2, 2);
-        }
-
-        if (l == 1) {
-            par1World.setBlockMetadataWithNotify(par2, par3, par4, 5, 2);
-        }
-
-        if (l == 2) {
-            par1World.setBlockMetadataWithNotify(par2, par3, par4, 3, 2);
-        }
-
-        if (l == 3) {
-            par1World.setBlockMetadataWithNotify(par2, par3, par4, 4, 2);
-        }
-
-        if (par6ItemStack.hasDisplayName()) {
-//            ((TileEntityFurnace)par1World.getBlockTileEntity(par2, par3, par4)).setGuiDisplayName(par6ItemStack.getDisplayName());
-        }
-    }
-
-    /**
-     * Called on server worlds only when the block has been replaced by a
-     * different block ID, or the same block with a different metadata value,
-     * but before the new metadata value is set. Args: World, x, y, z, old block
-     * ID, old metadata
-     */
-    public void breakBlock(World par1World, int par2, int par3, int par4, int par5, int par6) {
-        if (!keepFurnaceInventory) {
-            TEReplicator tileentityfurnace = (TEReplicator) par1World.getBlockTileEntity(par2, par3, par4);
-
-            if (tileentityfurnace != null) {
-                for (int j1 = 0; j1 < tileentityfurnace.getSizeInventory(); ++j1) {
-                    ItemStack itemstack = tileentityfurnace.getStackInSlot(j1);
-
-                    if (itemstack != null) {
-                        float f = this.furnaceRand.nextFloat() * 0.8F + 0.1F;
-                        float f1 = this.furnaceRand.nextFloat() * 0.8F + 0.1F;
-                        float f2 = this.furnaceRand.nextFloat() * 0.8F + 0.1F;
-
-                        while (itemstack.stackSize > 0) {
-                            int k1 = this.furnaceRand.nextInt(21) + 10;
-
-                            if (k1 > itemstack.stackSize) {
-                                k1 = itemstack.stackSize;
-                            }
-
-                            itemstack.stackSize -= k1;
-                            EntityItem entityitem = new EntityItem(par1World, (double) ((float) par2 + f), (double) ((float) par3 + f1), (double) ((float) par4 + f2), new ItemStack(itemstack.itemID, k1, itemstack.getItemDamage()));
-
-                            if (itemstack.hasTagCompound()) {
-                                entityitem.getEntityItem().setTagCompound((NBTTagCompound) itemstack.getTagCompound().copy());
-                            }
-
-                            float f3 = 0.05F;
-                            entityitem.motionX = (double) ((float) this.furnaceRand.nextGaussian() * f3);
-                            entityitem.motionY = (double) ((float) this.furnaceRand.nextGaussian() * f3 + 0.2F);
-                            entityitem.motionZ = (double) ((float) this.furnaceRand.nextGaussian() * f3);
-                            par1World.spawnEntityInWorld(entityitem);
-                        }
-                    }
-                }
-
-                par1World.func_96440_m(par2, par3, par4, par5);
-            }
-        }
-
-        super.breakBlock(par1World, par2, par3, par4, par5, par6);
-    }
-
-    /**
-     * If this returns true, then comparators facing away from this block will
-     * use the value from getComparatorInputOverride instead of the actual
-     * redstone signal strength.
-     */
-    public boolean hasComparatorInputOverride() {
-        return true;
-    }
-
-    /**
-     * If hasComparatorInputOverride returns true, the return value from this is
-     * used instead of the redstone signal strength when this block inputs to a
-     * comparator.
-     */
-    public int getComparatorInputOverride(World par1World, int par2, int par3, int par4, int par5) {
-        return Container.calcRedstoneFromInventory((IInventory) par1World.getBlockTileEntity(par2, par3, par4));
-    }
-
-    @SideOnly(Side.CLIENT)
-
-    /**
-     * only called by clickMiddleMouseButton , and passed to
-     * inventory.setCurrentItem (along with isCreative)
-     */
-    public int idPicked(World par1World, int par2, int par3, int par4) {
-        return Block.furnaceIdle.blockID;
-    }
 }
